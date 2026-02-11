9aa2c1090fc472aa448375a2761339a809dba7de
bogy:3 <palotas.boglarka@diak.szbi-pg.hu>
2026-02-11 10:09:51 +0100

    kesz

--- labdarugo_jatekosok.py
+++ labdarugo_jatekosok.py
@@ -14,10 +14,71 @@ Olvasd be a labdarugok.txt adatait, majd oldd meg az alábbi feladatokat!
 A megoldott feladatokat a kiirt_adatok nevű mappában hozd létre statisztika.txt néven!
 """
 
+labda_rugok = []
+with open('beolvasando_adatok/labdarugok.txt', 'r', encoding='utf-8') as forrasfajl:
+    next(forrasfajl)
+    for sor in forrasfajl:
+        adatok = sor.strip().split(';')
+        nev = adatok[0]
+        csapat= adatok[1]
+        golszam = int(adatok[2])
+        merkozesekszama = int(adatok[3])
+      
+        labdarugo = {'nev': nev,
+                'csapat': csapat,
+                'golszam': golszam,
+                'merkozesekszama': merkozesekszama}
+        labda_rugok.append(labdarugo)
 
-print("A beolvasott fájlban összesen ____ játékos szerepel.")
-print("A legkevesebb gólt szerző játékos: ____")
-print("A legtöbb gólt szerző játékos: ____")
-print("A legtöbb mérkőzést játszó játékos: ____")
-print("Az átlagos gólszám: ____")
+print(f'{labda_rugok=}')
+
+
+#1
+print(f"A beolvasott fájlban összesen {len(labda_rugok)} játékos szerepel.")
+
+
+#2
+legkevesebb_gol = labda_rugok[0]["golszam"]
+legkevesebb = labda_rugok[0]["nev"]
+
+for labdarugo in labda_rugok:
+    if labdarugo["golszam"] < legkevesebb_gol:
+        legkevesebb_gol = labdarugo["golszam"]
+        legkevesebb = labdarugo["nev"]
+
+print(f"A legkevesebb gólt szerző játékos: {legkevesebb} ")
+
+
+#3
+legtobb_gol = labda_rugok[0]["golszam"]
+legtobb = labda_rugok[0]["nev"]
+
+for labdarugo in labda_rugok:
+    if labdarugo["golszam"] > legtobb_gol:
+        legtobb_gol = labdarugo["golszam"]
+        legtobb = labdarugo["nev"]
+
+print(f"A legtöbb gólt szerző játékos: {legtobb}")
+
+
+#4
+legtobb_mer = labda_rugok[0]["merkozesekszama"]
+legtobb_m = labda_rugok[0]["nev"]
+
+for labdarugo in labda_rugok:
+    if labdarugo["merkozesekszama"] > legtobb_mer:
+        legtobb_mer = labdarugo["merkozesekszama"]
+        legtobb_m = labdarugo["nev"]
+
+print(f"A legtöbb mérkőzést játszó játékos: {legtobb_m}")
+
+#5
+golaszamok = []
+for labdarugo in labda_rugok:
+    golaszamok.append(golszam)
+atlag = sum(golaszamok) / len(golaszamok)
+
+print(f"Az átlagos gólszám: {atlag}")
+
+#6
 print("***A legtöbb gólt szerző csapat: ____")
